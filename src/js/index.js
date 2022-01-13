f3c9846e01fa335c7aab4cd73c9c91a9d9ed96bc
EnDrOLl_ <66158890+34663@users.noreply.github.com>
2022-01-13 17:37:31 +0900

    Update Theme
    明るい色にした

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,21 @@
-const { ipcRenderer } = require('electron');
-const ipc = ipcRenderer;
+// const { ipcRenderer } = require('electron');
+// const ipc = ipcRenderer;
 
-const minimize = document.getElementById('minimize');
-const close = document.getElementById('close');
-minimize.addEventListener('click', () => {
-    ipc.send('minimizeApp');
-});
-close.addEventListener('click', () => {
-    ipc.send('closeApp');
+// const minimize = document.getElementById('minimize');
+// const close = document.getElementById('close');
+// minimize.addEventListener('click', () => {
+//     ipc.send('minimizeApp');
+// });
+// close.addEventListener('click', () => {
+//     ipc.send('closeApp');
+// });
+
+document.querySelectorAll('.slider').forEach((it) => {
+    it.outerHTML = `<label><span class="slider-value">Delay: ${it.value}</span>` + it.outerHTML + '</label>';
 });
 
-const slider = document.querySelector("input");
-slider.oninput = (() => {
-    
+document.querySelectorAll('.slider').forEach((it) => {
+    it.addEventListener('input', () => {
+        it.parentElement.querySelector('.slider-value').innerHTML = `Delay: ${it.value}`;
+    });
 });
