6e4a2a48a1c20d9b71198f27a9dc068638ba46b7
Toshimichi0915 <main@toshimichi.net>
2022-01-15 03:43:08 +0900

    アカウント読み込み機能の追加

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,14 @@ document.querySelectorAll('.slider').forEach((it) => {
     });
 });
 
-document.querySelectorAll('.sidebar-item').forEach((it)=> {
-    it.addEventListener('click', ()=> {
+document.querySelectorAll('.sidebar-item').forEach((it) => {
+    it.addEventListener('click', () => {
         let payloadId = it.getAttribute("data-payload")
 
-        document.querySelectorAll('.payload-container').forEach((container)=> {
+        document.querySelectorAll('.payload-container').forEach((container) => {
             container.classList.add('payload-container-hidden')
         })
-        document.querySelectorAll('.item-text').forEach((text)=> {
+        document.querySelectorAll('.item-text').forEach((text) => {
             text.classList.remove('current')
         })
 
@@ -23,4 +23,46 @@ document.querySelectorAll('.sidebar-item').forEach((it)=> {
         payload.classList.remove('payload-container-hidden')
         it.querySelector('.item-text').classList.add('current')
     })
+})
+
+document.querySelectorAll('.file-selector').forEach((it) => {
+    it.addEventListener('click', () => {
+        let fileInput = document.querySelector("#" + it.getAttribute('data-file'))
+        fileInput.click()
+    })
+})
+
+function get_file() {
+
+}
+
+let inputLoadAccounts = document.querySelector('#input-load-accounts')
+inputLoadAccounts.addEventListener('change', () => {
+    let reader = new FileReader()
+    reader.onload = () => {
+        let accountList = document.querySelector('#accounts-list')
+        accountList.innerHTML = ''
+        reader.result.split('\n').forEach((line) => {
+            let newId = (Math.random() + 1).toString(36).substring(10);
+            let split = line.split(':')
+            if (split.length == 2) {
+                newItem  =`
+                <li class="item">
+                    <div class="item-container">
+                        <input type="checkbox" id="${newId}">
+                        <label class="frozen" for="${newId}">${split[0]}</label>
+                        <div class="info">
+                            <p>
+                                Link:
+                                <span>twitter.com</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                `
+                accountList.innerHTML += newItem
+            }
+        })
+    }
+    reader.readAsText(inputLoadAccounts.files[0])
 })
